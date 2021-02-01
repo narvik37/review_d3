10027c715064aa11e79edb222795ac18161eaeab
Kyuhyeon Lee <winnaque@gmail.com>
2021-02-01 14:44:25 +0900

    first commit

--- cookies/views.py
+++ cookies/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
 # Create your views here.
+from django.contrib.auth.hashers import make_password
 
 def index(req):
-    return HttpResponse("<h1>cookies</h1>")
+    if req.method == 'GET' :
+        print('###########')
+        res = render(req, "signup.html")
+        res.delete_cookie('pw')
+        return res
+
+    if req.COOKIES.get('pw') is not None:
+        en_pw = req.COOKIES.get('pw')
+        print(en_pw)
+
+    if req.method == 'POST' :
+        pw = req.POST.get('password','')
+        print(pw)
+        res = render(req, "signup.html")
+        res.set_cookie('pw', make_password(pw))
+        return res
+
+    return render(req ,"signup.html")
